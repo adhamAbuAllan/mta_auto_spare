bfbb267915369d3e99d91de43034154dc4769656
needf <needforspeedun3@gmail.com>
2026-05-05 23:34:25 +0300

    Point privacy policy link to public backend

--- lib/constants/app_links.dart
+++ lib/constants/app_links.dart
@@ -1,5 +1,7 @@
+import 'api_constants.dart';
+
 abstract final class AppLinks {
   static final Uri privacyPolicy = Uri.parse(
-    'https://adhamAbuAllan.github.io/mta_auto_spare/',
-  );
+    ApiConstants.baseUrl,
+  ).resolve('/privacy-policy/');
 }
